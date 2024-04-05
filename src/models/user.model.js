f8e193c7b471c9a21b491b9dcc6ec8ad5d5c75c0
VDucDung <dunga3qv2@gmail.com>
2024-04-05 22:09:53 +0700

    ✨ Create user model

---
+++ src/models/user.model.js
@@ -0,0 +1,57 @@
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      default: '',
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'user'],
+      default: 'user',
+    },
+    isVerify: {
+      type: Boolean,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+userSchema.statics.isEmailTaken = async function (email, excludeUserId) {
+  const user = await this.findOne({ email, _id: { $ne: excludeUserId } });
+  return !!user;
+};
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+userSchema.methods.isPasswordMatch = async function (password) {
+  const user = this;
+  return await bcrypt.compare(password, user.password);
+};
+const User = mongoose.model('User', userSchema);
+module.exports = User;
