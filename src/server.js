b8659daff92126e2bef483bff4a3455aac25b521
VDucDung <dunga3qv2@gmail.com>
2024-04-05 17:30:05 +0700

    🎉 Init project

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server Blog App is running 🎉");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
