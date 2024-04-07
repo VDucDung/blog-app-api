da00bd84e6d83241db2e9cf27281b08a4bc8d700
VDucDung <dunga3qv2@gmail.com>
2024-04-07 16:28:13 +0700

    🌱 Create category model

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,4 @@
 module.exports.User = require('./user.model');
 module.exports.Post = require('./post.model');
 module.exports.Comment = require('./comment.model');
+module.exports.Category = require('./category.model');
