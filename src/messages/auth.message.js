fbfa485855bd9380031e18c0e228e81c4fc0a443
VDucDung <dunga3qv2@gmail.com>
2024-04-08 11:25:13 +0700

    ➕ Add some error messages

--- src/messages/auth.message.js
+++ src/messages/auth.message.js
@@ -1,4 +1,5 @@
 const { i18nService } = require('../config');
+
 const authMessage = () => {
   return {
     INVALID_LOGIN: i18nService.translate('auth', 'invalidLogin'),
@@ -8,6 +9,11 @@ const authMessage = () => {
     INVALID_TOKEN: i18nService.translate('auth', 'invalidToken'),
     UNAUTHORIZED: i18nService.translate('auth', 'unauthorized'),
     FORBIDDEN: i18nService.translate('auth', 'forbidden'),
+    TOKEN_EXPIRED: i18nService.translate('auth', 'tokenExpired'),
+    GET_ME_SUCCESS: i18nService.translate('auth', 'getMeSuccess'),
+    INVALID_PASSWORD: i18nService.translate('auth', 'invalidPassword'),
+    CHANGE_PASSWORD_SUCCESS: i18nService.translate('auth', 'changePasswordSuccess'),
+    UPDATE_ME_SUCCESS: i18nService.translate('auth', 'updateMeSuccess'),
   };
 };
 
