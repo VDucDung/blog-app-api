d21316757bc20fcdce6e2ba87e807073ba5d79ee
VDucDung <dunga3qv2@gmail.com>
2024-04-30 10:47:15 +0700

    ✅ Update post model

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -2,36 +2,25 @@ const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema(
   {
-    title: {
-      type: String,
-      trim: true,
-      required: true,
-      unique: true,
-    },
-    desc: {
-      type: String,
-      trim: true,
-      required: true,
-      unique: true,
-    },
-    image: {
-      type: String,
-      required: true,
-      default: '',
-    },
-    categories: {
-      type: Array,
-    },
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
+    title: { type: String, required: true },
+    caption: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    body: { type: Object, required: true },
+    image: { type: String, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    tags: { type: [String] },
+    categories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],
   },
   {
     timestamps: true,
   },
 );
+
+postSchema.virtual('comments', {
+  ref: 'Comment',
+  localField: '_id',
+  foreignField: 'postId',
+});
 
 const Post = mongoose.model('Post', postSchema);
 module.exports = Post;
