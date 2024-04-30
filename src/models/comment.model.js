d6459324724a5e7ee0c9051d692324c3bb7d4e51
VDucDung <dunga3qv2@gmail.com>
2024-04-30 10:48:20 +0700

    ✅ Update comment model

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -2,25 +2,23 @@ const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema(
   {
-    comment: {
-      type: String,
-      required: true,
-    },
-    postId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Post',
-      required: false,
-    },
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
+    comment: { type: String, required: true },
+    postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: false },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    check: { type: Boolean, default: false },
+    parent: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment', default: null },
+    replyOnUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
   },
   {
     timestamps: true,
   },
 );
+
+commentSchema.virtual('replies', {
+  ref: 'Comment',
+  localField: '_id',
+  foreignField: 'parent',
+});
 
 const Comment = mongoose.model('Comment', commentSchema);
 module.exports = Comment;
