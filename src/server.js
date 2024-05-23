e494e6fe59ae4c116c2804e22ce2fadc814005a8
VDucDung <dunga3qv2@gmail.com>
2024-05-23 23:53:12 +0700

    🐛 Fix the error of not being able to access headers from the client

--- src/server.js
+++ src/server.js
@@ -12,6 +12,10 @@ const baseRouter = require('./routes/base.route');
 
 const app = express();
 
+const corsOptions = {
+  exposedHeaders: 'authorization',
+};
+
 app.set('trust proxy', 1);
 
 app.use(rateLimitApp);
@@ -19,7 +23,7 @@ app.use(helmet());
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
 app.options('*', cors());
 
 app.use((req, res, next) => {
